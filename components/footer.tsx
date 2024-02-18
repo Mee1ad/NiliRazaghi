426dcf23e69a9146c255fb2fddd150c13593741b
Soheil <itsupport@londonist.co.uk>
2024-02-18 17:27:36 +0300

    Update typography style for author's name in footer
    Changed the styling of the author's name in the footer component. The text is now bold and has been given some left margin to make it more distinguishable and visually appealing.

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,11 @@ export const Footer = () => {
     return (
         <div className="flex flex-col py-10 items-center bg-black text-gray-50">
             <h4 className="text-3xl"> Nili Razaghi</h4>
-            <h6 className="text-xs pt-2 text-gray-300">Designed With ❤️ by Soheil Ravasani</h6>
+            <h6 className="text-xs pt-2 text-gray-300">Designed With ❤️ by
+                <span className="font-bold ml-1">
+                    Soheil Ravasani
+                </span>
+            </h6>
         </div>
     )
 }
