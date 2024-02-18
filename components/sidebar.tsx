a2f3c44131f4137cf5144d628c471c74bcd9d662
Soheil <itsupport@londonist.co.uk>
2024-02-18 13:17:53 +0300

    fix sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,6 +5,7 @@ import { PiInstagramLogoLight } from "react-icons/pi";
 import { CiLinkedin } from "react-icons/ci";
 // import {fontjosefin} from "@/config/fonts";
 import clsx from "clsx";
+import {ThemeSwitch} from "@/components/theme-switch";
 
 
 export const Sidebar = () => {
@@ -17,31 +18,33 @@ export const Sidebar = () => {
         {href: "contact", text: "Contact"}
     ]
     return (
-        <div className="w-23">
-        <div className="px-8 py-32 w-23 h-full fixed"
-             style={{boxShadow: "1px 1px 20px 0 rgba(153, 153, 153, 0.32)"}}>
-            <section className="flex flex-col items-center mb-14">
-                <h1 className="text-3xl font-bold sticky top-0">Nili Razaghi</h1>
-                <h3>Photographer</h3>
-            </section>
-            <section className="flex-col flex">
-                {links.map(link => (
-                    <Link
-                        key={link.href}
-                        href={link.href}
-                        className="border-b-1.5 leading-[3]"
-                        color="foreground"
-                    >
-                        {link.text}
-                    </Link>
-                ))}
-            </section>
-            <footer className="flex text-3xl mt-20 justify-center gap-2">
-                <PiTelegramLogoThin />
-                <PiInstagramLogoLight />
-                <CiLinkedin />
-            </footer>
-        </div>
+        <div className="w-23 z-50">
+            <div className="px-8 py-32 w-23 h-full fixed z-50 text-center"
+                 style={{boxShadow: "1px 1px 20px 0 rgba(153, 153, 153, 0.32)"}}>
+                <section className="flex flex-col items-center mb-14">
+                    <h1 className="text-3xl font-bold sticky top-0">Nili Razaghi</h1>
+                    <h3>Photographer</h3>
+                    <ThemeSwitch/>
+                </section>
+                <section className="flex-col flex">
+                    {links.map(link => (
+                        <Link
+                            key={link.href}
+                            href={link.href}
+                            className="border-b-1.5 leading-[3]"
+                            color="foreground"
+                        >
+                            {link.text}
+                        </Link>
+                    ))}
+                </section>
+                <footer className="flex text-3xl mt-20 justify-center gap-2">
+                    <PiTelegramLogoThin/>
+                    <PiInstagramLogoLight/>
+                    <CiLinkedin/>
+                </footer>
+                <h6 className="text-sm pt-5">With ❤️ by Soheil Ravasani</h6>
+            </div>
         </div>
     )
 }
