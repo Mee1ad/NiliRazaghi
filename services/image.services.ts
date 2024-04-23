1f32e5594dedddef98d46ce701e551dfa3af633e
Soheil <soheilravasani@gmail.com>
2024-04-23 14:57:07 +0300

    Refactor image services and add multiple images insertion function
    Updated constants in image.services.ts file for better clarity and consistency. Additionally, a new function to insert multiple images to the database has been added. The function for retrieving bucket images was also refined to filter out images without metadata.

--- services/image.services.ts
+++ services/image.services.ts
@@ -1,26 +1,42 @@
 import {DatabaseImage} from "@/interface/database_image";
 import supabase from "@/config/supabase_service";
-import {BUCKET_NAME, IMAGE_TABLE, PAGE_TABLE} from "@/config/consts";
+import {IMAGE_BUCKET, IMAGE_TABLE, PAGE_TABLE} from "@/config/consts";
 import {Page} from "@/interface";
+import {BucketImage, BucketDirectory} from "@/interface/image.interface";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+
+export const insertImages = async (images: DatabaseImage[]) => {
+    const {error} = await supabase
+        .from(IMAGE_TABLE)
+        .insert(images)
+    if (error) {
+        throw error
+    }
+    return images
+}
 
 export const insertImage = async (image: DatabaseImage) => {
     const {error} = await supabase
         .from(IMAGE_TABLE)
-        .insert([image])
+        .insert(image)
     if (error) {
         throw error
     }
     return image
 }
+
 export const fetchBucketImages = async (pageName: Page["name"]) => {
-    const {data: bucketImages, error: bucketImagesError} = await supabase
+    let {data: bucketImages, error: bucketImagesError} = await supabase
         .storage
-        .from(BUCKET_NAME)
+        .from(IMAGE_BUCKET)
         .list(pageName)
     if (bucketImagesError) {
         throw bucketImagesError
     }
-    return bucketImages
+    const filteredBucketImages: BucketImage[] | undefined = bucketImages?.filter(image => image.metadata != null)
+
+    return filteredBucketImages as BucketImage[]
 }
 export const fetchPageImages = async (pageId: Page["id"]) => {
     const {data: dbImage, error: dbImageError} = await supabase
@@ -45,11 +61,19 @@ export const fetchPageByName = async (pageName: Page["name"]) => {
 export const fetchPublicImageUrl = (path: string) => {
     const {data: publicImageUrl} = supabase
         .storage
-        .from(BUCKET_NAME)
+        .from(IMAGE_BUCKET)
         .getPublicUrl(path)
     return publicImageUrl.publicUrl
 }
-
 export const fetchFileName = (fileName: string) => {
     return fileName.split('.')[0]
+}
+export const imageToDbImage = (bucketImage, page) => {
+    return {
+        alt: bucketImage.name.split('.')[0],
+        bucket_image_id: bucketImage.id,
+        page_id: page.id,
+        order: 99,
+        url: fetchPublicImageUrl(`${page.name}/${bucketImage.name}`)
+    }
 }
