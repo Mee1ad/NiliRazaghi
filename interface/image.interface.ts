0bce7a8e95fa27efe53f41c963852b357aa40d83
Soheil <soheilravasani@gmail.com>
2024-04-23 14:55:43 +0300

    Add BucketImage and BucketDirectory interfaces
    Two new interfaces, BucketImage and BucketDirectory have been added to the image interface. These interfaces outline key identifying information and metadata properties, that will be used to manage bucket-related storage and manipulation functionalities.

---
+++ interface/image.interface.ts
@@ -0,0 +1,26 @@
+export interface BucketImage {
+    name: string,
+    id: string,
+    updated_at: string,
+    created_at: string,
+    last_accessed_at: string,
+    metadata: {
+        eTag?: string,
+        size?: number,
+        mimetype?: string,
+        cacheControl?: string,
+        lastModified?: string,
+        contentLength?: number,
+        httpStatusCode?: number
+    }
+
+}
+
+export interface BucketDirectory {
+    name: string,
+    id?: string,
+    updated_at?: string,
+    created_at?: string,
+    last_accessed_at?: string,
+    metadata?: object
+}
