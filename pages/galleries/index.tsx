e1f32832d4055b61ef1470eabb1f14b2bae05696
Soheil <soheilravasani@gmail.com>
2024-04-27 10:43:33 +0300

    Refactor galleries.tsx and add card components
    The file galleries.tsx is renamed to galleries/index.tsx, contributing to better directory structure. The display of galleries is changed from previously just images to now making use of Card components from @nextui-org/react, for a more structured and informative display of gallery contents. In each card, there's a link leading to the specific gallery view.

---
+++ pages/galleries/index.tsx
@@ -0,0 +1,97 @@
+import {FC} from "react";
+import {REVALIDATE} from "@/config/consts";
+import {fetchBucketGalleries, fetchGalleries, fetchGalleryPage, populateGalleries} from '@/services/gallery.services'
+import {fetchBucketImages, fetchPageImages} from "@/services/image.services";
+import {BucketImage} from "@/interface/image.interface";
+import {title} from "@/components/primitives";
+import {Button, Card, CardBody, CardFooter, CardHeader, Image} from "@nextui-org/react";
+import Layout from "@/components/Layout";
+import {DatabaseImage} from "@/interface/database_image";
+import NextLink from "next/link";
+
+const GalleryPage: FC<{ galleries: DatabaseImage[] }> = ({galleries}) => {
+    return (
+        <Layout>
+            <div className="text-center py-20">
+                <title className={title()}>Gallery</title>
+                <h1 className="text-3xl font-bold">Portfolio</h1>
+                <section className="columns-1 sm:columns-2 md:columns-3 gap-10 p-10">
+                    {
+                        galleries.map((gallery) =>
+                            <div key={gallery.bucket_image_id} className="mb-10">
+                                <NextLink href={`/galleries/${gallery.alt}`}>
+                                    {/*<Card className="py-4">*/}
+                                    {/*    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">*/}
+                                    {/*        <h4 className="font-bold text-large">{gallery.alt}</h4>*/}
+                                    {/*    </CardHeader>*/}
+                                    {/*    <CardBody className="overflow-visible py-2">*/}
+                                    {/*        <Image*/}
+                                    {/*            shadow="sm"*/}
+                                    {/*            loading="eager"*/}
+                                    {/*            isBlurred={true}*/}
+                                    {/*            width="100%"*/}
+                                    {/*            radius="none"*/}
+                                    {/*            alt="test"*/}
+                                    {/*            src={gallery.url}/>*/}
+                                    {/*    </CardBody>*/}
+                                    {/*</Card>*/}
+
+                                    {/*<Card shadow="sm" isPressable onPress={() => console.log("item pressed")}>*/}
+                                    {/*    <CardBody className="overflow-visible p-0">*/}
+                                    {/*        <Image*/}
+                                    {/*                shadow="sm"*/}
+                                    {/*                loading="eager"*/}
+                                    {/*                isBlurred={true}*/}
+                                    {/*                width="100%"*/}
+                                    {/*                radius="none"*/}
+                                    {/*                alt="test"*/}
+                                    {/*                src={gallery.url}/>*/}
+                                    {/*    </CardBody>*/}
+                                    {/*    <CardFooter className="text-small justify-between">*/}
+                                    {/*        <b>{gallery.alt}</b>*/}
+                                    {/*        <p className="text-default-500">3.00$</p>*/}
+                                    {/*    </CardFooter>*/}
+                                    {/*</Card>*/}
+
+                                    <Card className="col-span-12 sm:col-span-4 h-[300px]">
+                                        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                                            <p className="text-tiny text-white/60 uppercase font-bold">Collection</p>
+                                            <h4 className="text-white font-medium text-large">{gallery.alt}</h4>
+                                        </CardHeader>
+                                        <Image
+                                            removeWrapper
+                                            className="z-0 w-full h-full object-cover"
+                                            shadow="sm"
+                                            loading="eager"
+                                            isBlurred={true}
+                                            width="100%"
+                                            radius="none"
+                                            alt="test"
+                                            src={gallery.url}/>
+                                    </Card>
+
+
+                                </NextLink>
+                            </div>
+                        )
+                    }
+                </section>
+            </div>
+        </Layout>
+    )
+}
+
+export default GalleryPage
+
+export async function getStaticProps() {
+    const pageName = "galleries"
+    const bucketGalleries: BucketImage[] | [] = await fetchBucketImages(pageName)
+    await populateGalleries(bucketGalleries)
+    const galleryPage = await fetchGalleryPage()
+    return {
+        props: {
+            galleries: galleryPage
+        },
+        revalidate: REVALIDATE
+    }
+}
