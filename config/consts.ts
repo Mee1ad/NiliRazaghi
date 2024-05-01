c49143aea3fc942f1e845d11403939c0c1125ed5
Soheil <soheilravasani@gmail.com>
2024-05-01 18:29:07 +0300

    Update title styling and disable main header in About page
    In the About page, the title tag's styling has been updated to include 'text-small !important' class for more explicit styling, and it is now placed within a div for structure. Additionally, the main header 'About Me' has been commented out and is currently not in use.

--- config/consts.ts
+++ config/consts.ts
@@ -3,4 +3,5 @@ export const IMAGE_BUCKET = "Nili Website"
 export const IMAGE_TABLE = "nili_image"
 export const PAGE_TABLE = "nili_page"
 export const FEEDBACK_TABLE = "nili_feedback"
+export const REELS_TABLE = "nili_reels"
 export const REVALIDATE = 60
