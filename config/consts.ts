87fca39ee80ae1f93a6e73bb0c9ad12757e693c4
Soheil <soheilravasani@gmail.com>
2024-05-11 12:09:53 +0300

    Add SALT constant to config constants
    A new constant named SALT has been added to the file config/consts.ts. This constant will be utilized for cryptographic functionalities.

--- config/consts.ts
+++ config/consts.ts
@@ -5,3 +5,4 @@ export const PAGE_TABLE = "nili_page"
 export const FEEDBACK_TABLE = "nili_feedback"
 export const REELS_TABLE = "nili_reels"
 export const REVALIDATE = 60
+export const SALT = "%^&G&^*gfuyiy98t7fg98gfyu"
