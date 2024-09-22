fe7beb330a17e57c834e8c3bd7d868710aa10789
Mee1ad <soheilravasani@gmail.com>
2024-09-22 12:32:33 +0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import Layout from "@/components/layout";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function MyApp({ Component, pageProps }:any) {
     return (
         <Layout>
+            <Analytics />
             <Component {...pageProps} />
         </Layout>
     )
